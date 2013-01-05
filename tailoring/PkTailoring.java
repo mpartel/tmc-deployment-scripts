91b192508e81e9753f5c12e4dd4c2a7c1f987d57
Martin Pärtel <martin.partel@gmail.com>
2013-01-05 08:20:33 +0200

    Build installers, use a staging dir, update all builds to 0.3.11.

--- tailoring/PkTailoring.java
+++ tailoring/PkTailoring.java
@@ -2,27 +2,27 @@ package fi.helsinki.cs.tmc.tailoring;
 
 import java.util.Locale;
 
-public class MoocTailoring extends DefaultTailoring {
+public class PkTailoring extends DefaultTailoring {
     @Override
     public String getDefaultServerUrl() {
-        return "http://tmc.mooc.fi/mooc";
+        return "http://tmc.mooc.fi/pk";
     }
     
     @Override
     public String getUpdateCenterTitle() {
-        return "TMC Mooc Edition Updates";
+        return "TMC Mooc PK Edition Updates";
     }
 
     @Override
     public String getUpdateCenterUrl() {
-        return "http://update.testmycode.net/tmc-netbeans_mooc/updates.xml";
+        return "http://update.testmycode.net/tmc-netbeans_pk/updates.xml";
     }
-
+    
     @Override
     public Locale[] getAvailableErrorMsgLocales() {
         return new Locale[] { new Locale("en"), new Locale("fi") };
     }
-
+    
     @Override
     public Locale getDefaultErrorMsgLocale() {
         return new Locale("fi");
