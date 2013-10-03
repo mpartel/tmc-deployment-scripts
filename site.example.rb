30ffa8e870c7370a9029780253786a2724b12232
Jarmo Isotalo <jamo@isotalo.fi>
2013-10-03 10:41:32 +0300

    adding support to build plugin and installer from different netbeans versions

--- site.example.rb
+++ site.example.rb
@@ -1,4 +1,5 @@
 
 # Copy this to site.rb and edit
-NB_DIR = '/opt/netbeans-7.2.1'
+NB_DIR_FOR_TMCNETBEANS = '/opt/netbeans-7.2.1'
+NB_DIR_FOR_PLUGIN = '/opt/netbeans-7.2.1'
 UPDATE_SITE = '/srv/www/update.testmycode.net'
