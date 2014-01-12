0c96f77cd5227dc9a6963a9efb3c7fdcf231799f
Martin Pärtel <martin.partel@gmail.com>
2014-01-12 04:57:54 +0200

    Plugin version to 0.6.0; build against NB 7.4.

--- site.example.rb
+++ site.example.rb
@@ -1,7 +1,7 @@
 
 # Copy this to site.rb and edit
-NB_DIR_FOR_TMCNETBEANS = '/opt/netbeans-7.2.1'
-NB_DIR_FOR_PLUGIN = '/opt/netbeans-7.2.1'
+NB_DIR_FOR_TMCNETBEANS = '/opt/netbeans-7.4'
+NB_DIR_FOR_PLUGIN = '/opt/netbeans-7.4'
 UPDATE_SITE = '/srv/www/update.testmycode.net'
 INSTALLER_SITE = '/srv/www/update.testmycode.net/installers'
 PACK200 = true
