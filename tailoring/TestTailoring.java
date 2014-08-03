b38e47909ccb2d35e410b17161c0c4ae085cc81a
Jarmo Isotalo <jamo@isotalo.fi>
2014-08-03 22:16:53 +0300

    NB 8 and newer versions + _test variant.

---
+++ tailoring/TestTailoring.java
@@ -0,0 +1,40 @@
+package fi.helsinki.cs.tmc.tailoring;
+
+import java.util.Locale;
+
+public class HyTailoring extends DefaultTailoring {
+    @Override
+    public String getDefaultServerUrl() {
+        return "https://tmc.mooc.fi/test";
+    }
+    
+    @Override
+    public boolean isSpywareEnabledByDefault() {
+        return true;
+    }
+
+    @Override
+    public boolean isDetailedSpywareEnabledByDefault() {
+        return true;
+    }
+    
+    @Override
+    public String getUpdateCenterTitle() {
+        return "TMC TEST Edition Updates";
+    }
+
+    @Override
+    public String getUpdateCenterUrl() {
+        return "http://update.testmycode.net/tmc-netbeans_test/updates.xml";
+    }
+    
+    @Override
+    public Locale[] getAvailableErrorMsgLocales() {
+        return new Locale[] { new Locale("en"), new Locale("fi") };
+    }
+    
+    @Override
+    public Locale getDefaultErrorMsgLocale() {
+        return new Locale("fi");
+    }
+}
