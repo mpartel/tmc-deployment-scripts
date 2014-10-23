c21aca1a218cf7e4baf78e8f46bb3d739ed5cf5b
Jarmo Isotalo <jamo@isotalo.fi>
2014-10-23 22:35:22 +0000

    Config for tktl deployed tmc version

---
+++ tailoring/HyTktlTailoring.java
@@ -0,0 +1,40 @@
+package fi.helsinki.cs.tmc.tailoring;
+
+import java.util.Locale;
+
+public class HyTktlTailoring extends DefaultTailoring {
+    @Override
+    public String getDefaultServerUrl() {
+        return "https://tmc.mooc.fi/hy";
+    }
+    
+    @Override
+    public boolean isSpywareEnabledByDefault() {
+        return true;
+    }
+
+    @Override
+    public boolean isDetailedSpywareEnabledByDefault() {
+        return true;
+    }
+    
+    @Override
+    public String getUpdateCenterTitle() {
+        return "TMC HY Edition Updates";
+    }
+
+    @Override
+    public String getUpdateCenterUrl() {
+        return "http://update.testmycode.net/tmc-netbeans_hy_tktl/updates.xml";
+    }
+    
+    @Override
+    public Locale[] getAvailableErrorMsgLocales() {
+        return new Locale[] { new Locale("en"), new Locale("fi") };
+    }
+    
+    @Override
+    public Locale getDefaultErrorMsgLocale() {
+        return new Locale("fi");
+    }
+}
